827879599dc961aca38157aadf1701c39a66b5f2
wlsdy <wlsdyd0638@gmail.com>
2024-07-10 18:32:52 +0900

    excel 출력 수정중

---
+++ SharedProject_IS_HeavyIndustry/Views/TabViews/HyungGangTabView.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace SharedProject_IS_HeavyIndustry.Views;
+
+public partial class HyungGangTabView : TabView
+{
+    public HyungGangTabView()
+    {
+        InitializeComponent();
+    }
+}

0f1a2d73912963c7d75c87dbe74e7815c1b25319
Suchacoolguy <coolguy55552@gmail.com>
2024-10-30 22:48:03 +0900

    엑셀 BOM 데이터 유닛 테스트

--- UnitTestArrangePartsService/UnitTest1.cs
+++ UnitTestArrangePartsService/UnitTest1.cs
@@ -1,17 +1,32 @@
+using ClosedXML.Excel;
+using DocumentFormat.OpenXml.Spreadsheet;
+using SharedProject_IS_HeavyIndustry.Models;
+using SharedProject_IS_HeavyIndustry.ViewModels;
+
 namespace UnitTestArrangePartsService;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public static void ArrangePartsService_DividePart_ReturnVoid()
     {
-        // Arrange
-        int expected = 1;
-        // Act
-        int actual = ReturnOne();
+        // Open the workbook
+        using (var workbook = new XLWorkbook(@"C:\Users\coolg\OneDrive\문서\아이에스중공업\240930 봉암리_가물량_BOM.xlsx"))
+        {
+            // Access the sheet by name
+            var sheet = workbook.Worksheet("Dwg-list");
 
-        // Assert
-        Assert.Equal(expected, actual);
+            // Retrieve parts from BOM
+            List<Part> partsFromBOM = ExcelDataReader.PartListFromExcel(sheet);
+
+            // Arrange
+            int expected = 1204;
+            // Act
+            int actual = partsFromBOM.Count; 
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
     }
     
     private int ReturnOne()
